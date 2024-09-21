1e37c04e12230c4cc88c2bbb231745ffeee9e4d4
ChaSuAn <100824715+ChaSuAn@users.noreply.github.com>
2024-09-21 13:07:24 +0800

    Add files via upload

--- SHAP_scatter.py
+++ SHAP_scatter.py
@@ -44,6 +44,8 @@ for i in range(num_rows):
         shap_value = shap_values.values[i, j]
         shap_values_lists[data_value].append(shap_value)
 plt.rcParams['font.family'] = 'Times New Roman'
+plt.rcParams.update({'font.size': 20})
+
 plt.figure(figsize=(18, 18))
 ax = plt.subplot(111, projection='polar')
 colors = cm.viridis(np.linspace(0, 1, len(shap_values_lists)))
@@ -56,9 +58,10 @@ for i, (shap_values_list, color) in enumerate(zip(shap_values_lists, colors)):
     r = r[mask]
     ax.scatter(theta, r, label=f'Level {i}', color=color, s=100, alpha=0.7, edgecolors='w', linewidth=0.5)
 
-r_ticks = np.linspace(0, max(max(r) for r in shap_values_lists), num=6)
+# 设置 r_ticks 并精确到小数点后一位
+r_ticks = np.linspace(0, round(max(max(r) for r in shap_values_lists), 1), num=6)
 ax.set_yticks(r_ticks)
-ax.set_yticklabels([f'{int(tick)}' for tick in r_ticks], fontsize=16)
+ax.set_yticklabels([f'{tick:.1f}' for tick in r_ticks], fontsize=20)  # 精确到小数点后一位，并增大字体
 
 ax.set_xticklabels([])
 ax.set_xticks([])
@@ -68,16 +71,16 @@ ax.legend(
     title='Sarcasm Level',
     bbox_to_anchor=(1.05, 1),
     loc='upper right',
-    fontsize=18,
+    fontsize=20,
     title_fontsize=20,
-    handletextpad=0.8,
+    handletextpad=0.6,
     labelspacing=0.6,
-    markerscale=1.2,
+    markerscale=1.0,
     borderpad=0.3,
     edgecolor='black'
 )
 
-title = 'SHAP Values for ' + sheetname + ' in Polar Coordinates'
-ax.set_title(title, fontsize=24)
-plt.savefig('Figure_6_sub1.jpg', dpi=1000, bbox_inches='tight')
-# plt.show()
+title = 'SHAP Values for ' + 'Real_Negative Group' + ' in Polar Coordinates'
+ax.set_title(title, fontsize=28)  # 增大标题字体
+plt.savefig('Figure_6_sub1.jpg', dpi=600, bbox_inches='tight')
+plt.show()
